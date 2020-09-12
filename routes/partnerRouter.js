085b31d5bb84e617f970fdc9d4dd8f69d9f1df47
Telly Lowe <57466776+TellyLowe@users.noreply.github.com>
2020-09-12 12:21:58 -0400

    workshop 1

---
+++ routes/partnerRouter.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const partnerRouter = express.Router();
+
+partnerRouter.use(bodyParser.json());
+
+partnerRouter.route('/partnerId')
+    .all((req, res, next) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        next();
+    })
+    .get((req, res) => {
+        res.end(
+            `Will send details of the promotion: ${req.params.partnerId} to you`
+        );
+    })
+    .post((req, res) => {
+        res.statusCode = 403;
+        res.end(
+            `POST operation not supported on /partners/${req.params.partnerId}`
+        );
+    })
+    .put((req, res) => {
+        res.write(`Updating the partner: ${req.params.partnerId}\n`);
+        res.end(`Will update the partner: ${req.body.name}
+            with description: ${req.body.description}`);
+    })
+    .delete((req, res) => {
+        res.end(`Deleting partner: ${req.params.partnerId}`);
+    });
+
+module.exports = partnerRouter;
